8ecc18bfc6f1e868b98916f18e9cdbfd8a123060
Scott B <scott.brugmans@gmail.com>
2023-08-03 13:34:44 +0200

    Small formatting updates

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,7 @@
-use std::{error::Error, time::Duration};
 use crossterm::event::{self, Event, KeyCode};
 use reqwest::blocking::get;
 use serde::Deserialize;
+use std::{error::Error, time::Duration};
 
 const BASE_URL: &str = "https://availability.ticketmaster.eu/api/v2/TM_NL/resale/";
 
@@ -22,15 +22,17 @@ struct Offer {
 }
 
 fn main() -> Result<(), Box<dyn Error>> {
-    // Initiliazing main variables
+    // Initiliazing main variable holding the events, and pre-filling it with two events
     let mut tm_events = vec![
         TMEvent {
+            // Some random event that has tickets
             id: "284753".to_string(),
-            offers: 0,
+            num_offers: 0,
         },
         TMEvent {
+            // Lowlands
             id: "280409".to_string(),
-            offers: 0,
+            num_offers: 0,
         },
     ];
     println!("Initialized, running program ...");
@@ -49,7 +51,7 @@ fn main() -> Result<(), Box<dyn Error>> {
                         println!("User forced polling of all events");
                         match update_events(&mut tm_events) {
                             // running update function & print a message for feedback. passing the main variable as mutable borrow, so the function can actually change the variable
-                            Ok(evnt) => println!("All events updated"),
+                            Ok(()) => println!("All events updated"),
                             Err(e) => println!("Error: {}", e),
                         }
                         println!("Data dump: {:?}", tm_events); // temp: data dump
@@ -72,7 +74,7 @@ fn update_events(evs: &mut Vec<TMEvent>) -> Result<(), Box<dyn Error>> {
             // checking if was succes
             Ok(n) => {
                 println!("Succesfully polled {}: there are {} offers", ev.id, n);
-                ev.offers = n as u32; // Updating EV; note that this will change the top variable struct as we have borrowed the variable
+                ev.num_offers = n as u32; // Updating EV; note that this will change the top variable struct as we have borrowed the variable
             }
             Err(err) => return Err(err),
         }
