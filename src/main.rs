f18e0c2fad483cacf8910011e1c11b551f4f6a2f
Scott B <scott.brugmans@gmail.com>
2023-08-05 21:06:14 +0200

    Refactored into seperate modules

--- src/main.rs
+++ src/main.rs
@@ -1,35 +1,17 @@
-use crossterm::event::{self, Event, KeyCode};
-use reqwest::blocking::get;
-use serde::Deserialize;
-use std::{error::Error, time::Duration};
+pub mod schema;
+use std::{error::Error};
 
-const BASE_URL: &str = "https://availability.ticketmaster.eu/api/v2/TM_NL/resale/";
-
-#[derive(Debug)]
-struct TMEvent {
-    id: String,
-    num_offers: u32,
-}
-
-#[derive(Deserialize, Debug)]
-struct TMResponse {
-    offers: Vec<Offer>,
-}
-
-#[derive(Deserialize, Debug)]
-struct Offer {
-    id: String,
-}
+// TicketMaster API: full response (JSON deserialized)
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Initiliazing main variable holding the events, and pre-filling it with two events
     let mut tm_events = vec![
-        TMEvent {
+        tm_poller::schema::TMEvent {
             // Some random event that has tickets
             id: "284753".to_string(),
             num_offers: 0,
         },
-        TMEvent {
+        tm_poller::schema::TMEvent {
             // Lowlands
             id: "280409".to_string(),
             num_offers: 0,
@@ -37,53 +19,23 @@ fn main() -> Result<(), Box<dyn Error>> {
     ];
     println!("Initialized, running program ...");
 
-    // Running main loop
-    'mainloop: loop {
-        while event::poll(Duration::default())? {
-            if let Event::Key(key_event) = event::read()? {
-                match key_event.code {
-                    KeyCode::Esc | KeyCode::Char('q') => {
-                        // User pressed ESC or 'q', breaking the main loop
-                        break 'mainloop;
-                    }
-                    KeyCode::Char('p') => {
-                        // User pressed 'p', forcing an update of the events
-                        println!("User forced polling of all events");
-                        match update_events(&mut tm_events) {
-                            // running update function & print a message for feedback. passing the main variable as mutable borrow, so the function can actually change the variable
-                            Ok(()) => println!("All events updated"),
-                            Err(e) => println!("Error: {}", e),
-                        }
-                        println!("Data dump: {:?}", tm_events); // temp: data dump
-                    }
-                    _ => {}
-                }
-            }
-        }
-    }
+    tm_poller::run(&mut tm_events).unwrap_or_else(|err| {
+        panic!("Main loop error: {}", err) //if something goes wrong in the main loop, panic! as this is an unknown issue
+    });
 
     // Preparing for exit
     println!("Quiting, bye!");
     Ok(())
 }
 
-fn update_events(evs: &mut Vec<TMEvent>) -> Result<(), Box<dyn Error>> {
-    for ev in evs.iter_mut() {
-        // Iterating over all the events
-        match poll_event(&ev.id) {
-            // checking if was succes
-            Ok(n) => {
-                println!("Succesfully polled {}: there are {} offers", ev.id, n);
-                ev.num_offers = n as u32; // Updating EV; note that this will change the top variable struct as we have borrowed the variable
-            }
-            Err(err) => return Err(err),
-        }
-    }
-    Ok(())
-}
+// Function update_events
+//
+// Updates the events
+//
+// Arguments
+//
+// * tm_events - a vector of TMEvent that holds all the current events that need to be polled, and polling data gets added to this vector. note we need to keep ownership in the main function, and borrow ownership to the functions below
+//
+// Returns an Ok(()) if no errors and an Box<error> in case there is an (underlying error)
 
-fn poll_event(id: &String) -> Result<usize, Box<dyn Error>> {
-    let request_url = format!("{}{}", BASE_URL, id);
-    let resp: TMResponse = get(request_url)?.json()?; // Doing the update, note the ? as this will propogate errors up!
-    Ok(resp.offers.len()) // return the number of offers (we could have passed the event too)
-}
+
